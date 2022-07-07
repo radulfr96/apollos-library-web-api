69debb49e0140b1d54bf8982f27596988067f5ea
Wade James Russell <wados.russell70@gmail.com>
2022-07-07 13:51:10 +1000

    made swagger default

--- ApollosLibrary.WebApi/Startup.cs
+++ ApollosLibrary.WebApi/Startup.cs
@@ -157,7 +157,11 @@ namespace ApollosLibrary.WebApi
             );
 
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
