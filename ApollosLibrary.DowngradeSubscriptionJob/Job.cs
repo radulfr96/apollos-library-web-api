6ea3be8fe2db46ce9d2071aa5ff134a782f4e240
Wade James Russell <wados.russell70@gmail.com>
2022-07-20 21:16:30 +1000

    added job to downgrade members

---
+++ ApollosLibrary.DowngradeSubscriptionJob/Job.cs
@@ -0,0 +1,46 @@
+﻿using ApollosLibrary.Domain;
+using ApollosLibrary.Domain.Enums;
+using Microsoft.Azure.WebJobs;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApollosLibrary.DowngradeSubscriptionJob
+{
+    public class Job
+    {
+        private readonly ApollosLibraryContext _context;
+        private readonly IClock _clock;
+
+        public Job(ApollosLibraryContext context, IClock clock)
+        {
+            _context = context;
+            _clock = clock;
+        }
+
+        [NoAutomaticTrigger]
+        public async Task Run(CancellationToken cancellationToken)
+        {
+            var instant = _clock.GetCurrentInstant();
+            var now = instant.InUtc().LocalDateTime;
+            var subscriptionsToDowngrade = await _context.Subscriptions
+                                                .Where(s => 
+                                                s.ExpiryDate < now 
+                                                &&  s.SubscriptionTypeId != (int)SubscriptionTypeEnum.Individual
+                                                && s.SubscriptionTypeId != (int)SubscriptionTypeEnum.Staff
+                                                )
+                                                .ToListAsync(cancellationToken);
+
+            foreach(var subscription in subscriptionsToDowngrade)
+            {
+                subscription.SubscriptionTypeId = (int)SubscriptionTypeEnum.SignedUp;
+            }
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ ApollosLibrary.DowngradeSubscriptionJob/Program.cs
@@ -0,0 +1,50 @@
+﻿using ApollosLibrary.Domain;
+using Microsoft.Azure.WebJobs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NodaTime;
+
+namespace ApollosLibrary.DowngradeSubscriptionJob
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorageQueues();
+                IConfiguration config = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json")
+                                .AddEnvironmentVariables()
+                                .Build();
+
+                b.Services.AddDbContext<ApollosLibraryContext>(options => options.UseNpgsql(config.GetSection("ConnectionString").Value, o => o.UseNodaTime()));
+                b.Services.AddDbContext<ApollosLibraryContext>();
+                b.Services.AddScoped<IClock>(c =>
+                {
+                    return SystemClock.Instance;
+                });
+            });
+
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+
+            var host = builder.Build();
+            using (host)
+            {
+                var jobHost = host.Services.GetRequiredService(typeof(IJobHost)) as JobHost;
+                await host.StartAsync();
+                await jobHost.CallAsync("Run");
+                await host.StopAsync();
+            }
+        }
+    }
+}
